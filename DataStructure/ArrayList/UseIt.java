c7a6302cf3dcab250a394f25c779cc02dd9a221e
purecloud75 <purecloud75@gmail.com>
2022-01-22 15:01:46 +0900

    01.22

--- DataStructure/ArrayList/UseIt.java
+++ DataStructure/ArrayList/UseIt.java
@@ -17,8 +17,7 @@ public class UseIt {
         numbers.add(1, 80); //1번인덱스공간에 80을 넣고싶다. 한칸씩 밀어서 넣는다.
         System.out.println(numbers);
 
-        numbers.get(2);
-        System.out.println(numbers);
+        System.out.println(numbers.get(2));
 
         numbers.remove(2);
         System.out.println(numbers);
@@ -27,16 +26,16 @@ public class UseIt {
         //Iterator 는 인터페이스이고, ArrayList 는 그것의 구현클래스이다.
         while (iterator.hasNext()) {
             int value = (int) iterator.next();
-            if(value == 30)
+            if (value == 30)
                 iterator.remove();
         }
 
         for (int value : numbers) {
-            System.out.println(value);
+            System.out.print(value + " ");
         }
 
         for (int i = 0; i < numbers.size(); i++) {
-            System.out.println(numbers.get(i));
+            System.out.print(numbers.get(i) + " ");
         }
 
     }
